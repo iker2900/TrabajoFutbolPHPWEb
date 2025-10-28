de5f231b40daebbf2f390b65e056d5700969cba7
Iker <iker29992@gmail>
2025-10-28 18:07:47 +0100

    En el PersistanceManager.php hemos añadido las variables necesarias para acceder a la base de datos por lo que el archivo JSON por ahora no deberia ser necesario

--- persistence/conf/PersistanceManager.php
+++ persistence/conf/PersistanceManager.php
@@ -1,1 +1,48 @@
 <?php
+
+class PersistentManager {
+
+
+    private $dbhost = 'localhost';
+    private $dbuser = 'root';
+    private $dbpass = '';
+    private $dbname = 'competicion';
+
+    private $conn;
+
+    /**
+     * Constructor. Llama al método de conexión.
+     */
+    public function __construct() {
+        $this->connect();
+    }
+
+    /**
+     * Crea la conexión mysql
+     */
+    public function connect() {
+        try {
+            $this->conn = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+        } catch (Exception $e) {
+
+            die("Error en la conexión de BBDD: " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Devuelve la conexión activa
+     */
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    /**
+     * Cierra la conexión
+     */
+    public function closeConnection() {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
+}
+?>
